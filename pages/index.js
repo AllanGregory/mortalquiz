8374eb6d706a3589f5624cda4c9920ba7bd60af1
unknown <alngregory@gmail.com>
2021-01-27 00:52:54 -0300

    Aula 01 - Criando o projeto com styled components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,52 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>Mortal Kombat</h1>
+            </Widget.Header>
+
+            <p>Texto teste</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz de outras pessoas</h1>
+
+            <p>Texto teste 2</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/AllanGregory" />
+    </QuizBackground>
+  );
 }
